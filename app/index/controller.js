a5258ec6c64ef684c1cb70751ba07198feb4c660
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-16 23:09:37 -0500

    Reset now works!

--- app/index/controller.js
+++ app/index/controller.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
+  reset: task(function*() {
+    const [first] = this.prompts;
+    this.set('prompts', []);
+    this.set('thinking', true);
+    yield timeout(1000);
+
+    this.set('thinking', false);
+    this.set('prompts', [first]);
+  }),
+
   answerQuestion: task(function*(answer) {
     this.set('thinking', true);
     const [prompt] = yield Ember.RSVP.all([
@@ -16,6 +26,10 @@ export default Ember.Controller.extend({
   actions: {
     getNextStep(answer) {
       this.get('answerQuestion').perform(answer);
+    },
+
+    restart() {
+      this.get('reset').perform();
     },
   },
 });
